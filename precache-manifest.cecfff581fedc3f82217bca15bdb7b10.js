ff9ec8ee2d4c7e39d7943a0da43e048f136839e4
Alex <alexiv1102@gmail.com>
2019-05-28 20:05:34 +0300

    Updates

---
+++ precache-manifest.cecfff581fedc3f82217bca15bdb7b10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86e673119b38dfc1efdc2a9af0ccd93",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "51df14f89733f7cb5f3e",
+    "url": "/2048/static/css/main.2f4bc18d.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/2048/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "51df14f89733f7cb5f3e",
+    "url": "/2048/static/js/main.fa50f5c4.chunk.js"
+  },
+  {
+    "revision": "e387bff5e0b18c0491fd",
+    "url": "/2048/static/js/runtime~main.f72a3c6a.js"
+  }
+]);
